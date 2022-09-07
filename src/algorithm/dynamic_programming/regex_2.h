dd7cdf9970d44034a4649930fc77a5a0f238dc5e
Bowei Zhang <chaoszbw@outlook.com>
2022-09-07 14:16:48 +0800

    3

---
+++ src/algorithm/dynamic_programming/regex_2.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>
+#include <limits.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+// NC44 通配符匹配
+// https://www.nowcoder.com/practice/e96f1a44d4e44d9ab6289ee080099322?tpId=117&tqId=37741&rp=1&ru=/exam/oj&qru=/exam/oj&sourceUrl=%2Fexam%2Foj%3FjudgeStatus%3D3%26page%3D1%26pageSize%3D50%26search%3D%26tab%3D%25E7%25AE%2597%25E6%25B3%2595%25E7%25AF%2587%26topicId%3D117&difficulty=undefined&judgeStatus=3&tags=&title=
+
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        
+    }
+}; 
