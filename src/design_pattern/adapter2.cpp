5699bf37f1cc5f6938d489e3b81e5a0f8a7529fa
Bowei Zhang <chaoszbw@outlook.com>
2022-09-08 17:08:42 +0800

    2

--- src/design_pattern/adapter2.cpp
+++ src/design_pattern/adapter2.cpp
@@ -9,11 +9,9 @@
  * The Target defines the domain-specific interface used by the client code.
  */
 class Target {
- public:
-  virtual ~Target() = default;
-  virtual std::string Request() const {
-    return "Target: The default target's behavior.";
-  }
+public:
+	virtual ~Target() = default;
+	virtual std::string Request() const { return "Target: The default target's behavior."; }
 };
 
 /**
@@ -22,50 +20,47 @@ class Target {
  * client code can use it.
  */
 class Adaptee {
- public:
-  std::string SpecificRequest() const {
-    return ".eetpadA eht fo roivaheb laicepS";
-  }
+public:
+	std::string SpecificRequest() const { return ".eetpadA eht fo roivaheb laicepS"; }
 };
 
 /**
  * The Adapter makes the Adaptee's interface compatible with the Target's
  * interface using multiple inheritance.
  */
-class Adapter : public Target, public Adaptee {
- public:
-  Adapter() {}
-  std::string Request() const override {
-    std::string to_reverse = SpecificRequest();
-    std::reverse(to_reverse.begin(), to_reverse.end());
-    return "Adapter: (TRANSLATED) " + to_reverse;
-  }
+class Adapter : public Target,
+                public Adaptee {
+public:
+	Adapter() {}
+	std::string Request() const override {
+		std::string to_reverse = SpecificRequest();
+		std::reverse(to_reverse.begin(), to_reverse.end());
+		return "Adapter: (TRANSLATED) " + to_reverse;
+	}
 };
 
 /**
  * The client code supports all classes that follow the Target interface.
  */
-void ClientCode(const Target *target) {
-  std::cout << target->Request();
-}
+void ClientCode(const Target* target) { std::cout << target->Request(); }
 
 int main() {
-  std::cout << "Client: I can work just fine with the Target objects:\n";
-  Target *target = new Target;
-  ClientCode(target);
-  std::cout << "\n\n";
-  Adaptee *adaptee = new Adaptee;
-  std::cout << "Client: The Adaptee class has a weird interface. See, I don't understand it:\n";
-  std::cout << "Adaptee: " << adaptee->SpecificRequest();
-  std::cout << "\n\n";
-  std::cout << "Client: But I can work with it via the Adapter:\n";
-  Adapter *adapter = new Adapter;
-  ClientCode(adapter);
-  std::cout << "\n";
+	std::cout << "Client: I can work just fine with the Target objects:\n";
+	Target* target = new Target;
+	ClientCode(target);
+	std::cout << "\n\n";
+	Adaptee* adaptee = new Adaptee;
+	std::cout << "Client: The Adaptee class has a weird interface. See, I don't understand it:\n";
+	std::cout << "Adaptee: " << adaptee->SpecificRequest();
+	std::cout << "\n\n";
+	std::cout << "Client: But I can work with it via the Adapter:\n";
+	Adapter* adapter = new Adapter;
+	ClientCode(adapter);
+	std::cout << "\n";
 
-  delete target;
-  delete adaptee;
-  delete adapter;
+	delete target;
+	delete adaptee;
+	delete adapter;
 
-  return 0;
+	return 0;
 }
